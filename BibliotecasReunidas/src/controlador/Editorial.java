25f1f830044f0267a39322430c301df4c951032c
DianaMaritza <dianamaritza.serna@juanxxiii.net>
2023-11-28 16:13:52 +0100

    he creado el proyecto en eclipse

---
+++ BibliotecasReunidas/src/controlador/Editorial.java
@@ -0,0 +1,16 @@
+package controlador;
+
+import javax.swing.JPanel;
+
+public class Editorial extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public Editorial() {
+
+	}
+
+}
---
+++ BibliotecasReunidas/src/controlador/Libro.java
@@ -0,0 +1,16 @@
+package controlador;
+
+import javax.swing.JPanel;
+
+public class Libro extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public Libro() {
+
+	}
+
+}
