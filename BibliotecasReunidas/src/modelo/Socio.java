70e1bafa5b1df94b61111a7e21b96d97d88be817
dan041223 <danielgarcia.barquilla@hotmail.com>
2023-11-28 16:52:09 +0100

    Atributos añadidos 3 clases
    Atributos añadidos en las clases Socio, Ubicacion y Usuario

--- BibliotecasReunidas/src/modelo/Socio.java
+++ BibliotecasReunidas/src/modelo/Socio.java
@@ -1,9 +1,91 @@
 package modelo;
 
-
-
 public class Socio  {
-
 	
-
+	private int id;
+	private String nombre;
+	private String fecha_creacion;
+	private String dni;
+	private int telefono;
+	private String calle;
+	private int codigo_postal;
+	private String email;
+	public enum TIPO_PAGO{
+		EFECTIVO,
+		TARJETA
+	}
+	private TIPO_PAGO tipo_pago;
+	
+	public Socio() {
+		super();
+	}
+	
+	public Socio(int id, String nombre, String fecha_creacion, String dni, int telefono, String calle,
+			int codigo_postal, String email, TIPO_PAGO tipo_pago) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.fecha_creacion = fecha_creacion;
+		this.dni = dni;
+		this.telefono = telefono;
+		this.calle = calle;
+		this.codigo_postal = codigo_postal;
+		this.email = email;
+		this.tipo_pago = tipo_pago;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getFecha_creacion() {
+		return fecha_creacion;
+	}
+	public void setFecha_creacion(String fecha_creacion) {
+		this.fecha_creacion = fecha_creacion;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public int getCodigo_postal() {
+		return codigo_postal;
+	}
+	public void setCodigo_postal(int codigo_postal) {
+		this.codigo_postal = codigo_postal;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public TIPO_PAGO getTipo_pago() {
+		return tipo_pago;
+	}
+	public void setTipo_pago(TIPO_PAGO tipo_pago) {
+		this.tipo_pago = tipo_pago;
+	}
 }
--- BibliotecasReunidas/src/modelo/Usuario.java
+++ BibliotecasReunidas/src/modelo/Usuario.java
@@ -1,9 +1,83 @@
 package modelo;
 
-
-
 public class Usuario {
 
+	private int id;
+	private String nombre;
+	private int telefono;
+	private String email;
+	private String calle;
+	private int codigo_postal;
+	private String dni;
+	public enum TIPO_PERFIL{
+		ADMINISTRADOR,
+		ADMINISTRATIVO
+	}
+	private TIPO_PERFIL tipo_perfil;
 	
-
+	public Usuario() {
+		super();
+	}
+	
+	public Usuario(int id, String nombre, int telefono, String email, String calle, int codigo_postal, String dni,
+			TIPO_PERFIL tipo_perfil) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.email = email;
+		this.calle = calle;
+		this.codigo_postal = codigo_postal;
+		this.dni = dni;
+		this.tipo_perfil = tipo_perfil;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public int getCodigo_postal() {
+		return codigo_postal;
+	}
+	public void setCodigo_postal(int codigo_postal) {
+		this.codigo_postal = codigo_postal;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public TIPO_PERFIL getTipo_perfil() {
+		return tipo_perfil;
+	}
+	public void setTipo_perfil(TIPO_PERFIL tipo_perfil) {
+		this.tipo_perfil = tipo_perfil;
+	}
 }
