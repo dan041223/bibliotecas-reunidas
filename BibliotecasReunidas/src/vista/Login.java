ed1257687de12e982ea3ec15f86c3816a55907e8
dan041223 <danielgarcia.barquilla@hotmail.com>
2023-11-28 21:00:21 +0100

    Prototipo Login añadido

---
+++ BibliotecasReunidas/src/vista/Login.java
@@ -0,0 +1,26 @@
+package vista;
+
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Login extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	public Login() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+
+	}
+
+}
