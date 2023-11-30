25618c9fb130709686329fab833f9155ed136f47
DianaMaritza <dianamaritza.serna@juanxxiii.net>
2023-11-30 13:04:59 +0100

    he creado el jPanel de Autor

--- BibliotecasReunidas/src/vista/AutorPanel.java
+++ BibliotecasReunidas/src/vista/AutorPanel.java
@@ -1,17 +1,89 @@
 package vista;
 
 import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class AutorPanel extends JPanel {
 	
 
 	private static final long serialVersionUID = 1L;
+	private JTextField textField_nombre;
+	private JTextField textField_Cod;
+	private JTextField textField_Nacionalidad;
+	private JTextField textField_Fecha;
 
 	/**
 	 * Create the panel.
 	 */
 	public AutorPanel() {
+		setLayout(null);
+		
+		JLabel codigoID = new JLabel("Codigo ID");
+		codigoID.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		codigoID.setBounds(145, 207, 80, 34);
+		add(codigoID);
+		
+		JLabel lblNombre = new JLabel("Nombre ");
+		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNombre.setBounds(145, 258, 60, 34);
+		add(lblNombre);
+		
+		JLabel lblNacionalidad = new JLabel("Nacionalidad");
+		lblNacionalidad.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNacionalidad.setBounds(145, 318, 80, 34);
+		add(lblNacionalidad);
+		
+		JLabel lblFecha = new JLabel("Fecha de Nacimiento");
+		lblFecha.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblFecha.setBounds(145, 386, 137, 34);
+		add(lblFecha);
+		
+		textField_nombre = new JTextField();
+		textField_nombre.setBounds(359, 263, 228, 34);
+		add(textField_nombre);
+		textField_nombre.setColumns(10);
+		
+		textField_Cod = new JTextField();
+		textField_Cod.setColumns(10);
+		textField_Cod.setBounds(359, 212, 228, 34);
+		add(textField_Cod);
+		
+		textField_Nacionalidad = new JTextField();
+		textField_Nacionalidad.setColumns(10);
+		textField_Nacionalidad.setBounds(359, 315, 228, 34);
+		add(textField_Nacionalidad);
+		
+		textField_Fecha = new JTextField();
+		textField_Fecha.setColumns(10);
+		textField_Fecha.setBounds(359, 370, 228, 34);
+		add(textField_Fecha);
+		
+		JLabel lblNewLabel_3 = new JLabel("AUTOR ");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_3.setBounds(391, 125, 94, 24);
+		add(lblNewLabel_3);
+		
+		JButton btnCrear = new JButton("Crear ");
+		btnCrear.setBounds(156, 547, 85, 34);
+		add(btnCrear);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.setBounds(292, 547, 85, 34);
+		add(btnModificar);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setBounds(550, 547, 85, 34);
+		add(btnEliminar);
+		
+		JButton btnBuscar_1 = new JButton("Buscar");
+		btnBuscar_1.setBounds(413, 547, 85, 34);
+		add(btnBuscar_1);
 
 	}
-
 }
--- BibliotecasReunidas/src/vista/LibroPanel.java
+++ BibliotecasReunidas/src/vista/LibroPanel.java
@@ -1,15 +1,111 @@
 package vista;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class LibroPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField textFieldCod;
+	private JTextField textField_Titulo;
+	private JTextField textField_Categoria;
+	private JTextField textField_Idioma;
+	private JTextField textField_Fecha;
+	private JTextField textField_ISBN;
 
 	/**
 	 * Create the panel.
 	 */
 	public LibroPanel() {
+		setLayout(null);
+		
+		JLabel lblCódigo = new JLabel("Código ID");
+		lblCódigo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCódigo.setBounds(199, 164, 89, 30);
+		add(lblCódigo);
+		
+		JLabel lblTitulo = new JLabel("Titulo");
+		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblTitulo.setBounds(199, 227, 89, 30);
+		add(lblTitulo);
+		
+		JLabel lblCategoriaDelLibro = new JLabel("Categoria Del Libro");
+		lblCategoriaDelLibro.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCategoriaDelLibro.setBounds(199, 277, 120, 30);
+		add(lblCategoriaDelLibro);
+		
+		JLabel lblIdioma = new JLabel("Idioma");
+		lblIdioma.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblIdioma.setBounds(199, 347, 120, 30);
+		add(lblIdioma);
+		
+		JLabel lblFechaDePublicacin = new JLabel("Fecha de Publicación");
+		lblFechaDePublicacin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblFechaDePublicacin.setBounds(199, 406, 156, 30);
+		add(lblFechaDePublicacin);
+		
+		JLabel lblIsbn = new JLabel("ISBN");
+		lblIsbn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblIsbn.setBounds(199, 469, 156, 30);
+		add(lblIsbn);
+		
+		textFieldCod = new JTextField();
+		textFieldCod.setBounds(487, 167, 212, 30);
+		add(textFieldCod);
+		textFieldCod.setColumns(10);
+		
+		textField_Titulo = new JTextField();
+		textField_Titulo.setColumns(10);
+		textField_Titulo.setBounds(487, 230, 212, 30);
+		add(textField_Titulo);
+		
+		textField_Categoria = new JTextField();
+		textField_Categoria.setColumns(10);
+		textField_Categoria.setBounds(487, 298, 212, 30);
+		add(textField_Categoria);
+		
+		textField_Idioma = new JTextField();
+		textField_Idioma.setColumns(10);
+		textField_Idioma.setBounds(487, 350, 212, 30);
+		add(textField_Idioma);
+		
+		textField_Fecha = new JTextField();
+		textField_Fecha.setColumns(10);
+		textField_Fecha.setBounds(487, 409, 212, 30);
+		add(textField_Fecha);
+		
+		textField_ISBN = new JTextField();
+		textField_ISBN.setColumns(10);
+		textField_ISBN.setBounds(487, 472, 212, 30);
+		add(textField_ISBN);
+		
+		JButton btnCrear = new JButton("Crear");
+		btnCrear.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		btnCrear.setBounds(100, 593, 99, 30);
+		add(btnCrear);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		btnModificar.setBounds(299, 593, 99, 30);
+		add(btnModificar);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		btnBuscar.setBounds(498, 593, 99, 30);
+		add(btnBuscar);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		btnEliminar.setBounds(697, 593, 99, 30);
+		add(btnEliminar);
+		
+		JLabel lblNewLabel = new JLabel("Libro");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lblNewLabel.setBounds(409, 89, 79, 45);
+		add(lblNewLabel);
 
 	}
 
